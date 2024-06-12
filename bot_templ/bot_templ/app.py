33c7687113cc7782deccdfd746497823d882ed67
Muhammad Zain Attiq <zainatteeq@gmail.com>
2024-06-12 05:58:56 +0500

    history bot added

---
+++ bot_templ/bot_templ/app.py
@@ -0,0 +1,154 @@
+import json
+import streamlit as st
+# from streamlit_chat import message
+from streamlit_chat import message
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage, ToolMessage
+import pathlib
+import textwrap
+from IPython.display import display
+from IPython.display import Markdown
+from itertools import zip_longest
+
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+
+openapi_key = st.secrets["OPENAI_API_KEY"]
+
+# Set streamlit page configuration
+st.set_page_config(page_title="AI Tutor")
+st.markdown(
+    """
+    <style>
+    .custom-title {
+        color: white; /* Text color */
+        font-size: 50px;
+        font-weight: bold;
+        text-align: center;
+        background-color: purple; /* Background color */
+        padding: 1px; /* Padding around the text */
+        border-radius: 10px; /* Rounded corners (optional) */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Use the custom CSS class for the title
+st.markdown('<p class="custom-title">AI Tutor</p>', unsafe_allow_html=True)
+
+
+# Initialize session state variables
+if 'entered_prompt' not in st.session_state:
+    st.session_state['entered_prompt'] = ""  # Store the latest user input
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []  # Store AI generated responses
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []  # Store past user inputs
+
+# Define function to submit user input
+def submit():
+    # Set entered_prompt to the current value of prompt_input
+    st.session_state.entered_prompt = st.session_state.prompt_input
+    # Clear prompt_input
+    st.session_state.prompt_input = ""
+# Example dummy function hard coded to return the same weather
+# In production, this could be your backend API or an external API
+def get_current_weather(location:str, unit:str="fahrenheit")->str:
+    """Get the current weather in a given location"""
+    if "tokyo" in location.lower():
+        return json.dumps({"location": "Tokyo", "temperature": "10", "unit": "celsius"})
+    elif "san francisco" in location.lower():
+        return json.dumps({"location": "San Francisco", "temperature": "72", "unit": "fahrenheit"})
+    elif "paris" in location.lower():
+        return json.dumps({"location": "Paris", "temperature": "22", "unit": "celsius"})
+    else:
+        return json.dumps({"location": location, "temperature": "unknown"})
+# Initialize the ChatOpenAI model
+chat = ChatOpenAI(
+    temperature=0.5,
+    model_name="gpt-3.5-turbo",
+    openai_api_key=openapi_key, 
+    max_tokens=100
+)
+def build_message_list():
+    """
+    Build a list of messages including system, human and AI messages.
+    """
+    # Start zipped_messages with the SystemMessage
+    zipped_messages = [SystemMessage(
+        content = """your name is AI Tutor. You are an AI Technical Expert for Artificial Intelligence, here to guide and assist students with their AI-related questions and concerns. Please provide accurate and helpful information, and always maintain a polite and professional tone.
+
+                1. Greet the user politely ask user name and ask how you can assist them with AI-related queries.
+                2. Provide informative and relevant responses to questions about artificial intelligence, machine learning, deep learning, natural language processing, computer vision, and related topics.
+                3. you must Avoid discussing sensitive, offensive, or harmful content. Refrain from engaging in any form of discrimination, harassment, or inappropriate behavior.
+                4. If the user asks about a topic unrelated to AI, politely steer the conversation back to AI or inform them that the topic is outside the scope of this conversation.
+                5. Be patient and considerate when responding to user queries, and provide clear explanations.
+                6. If the user expresses gratitude or indicates the end of the conversation, respond with a polite farewell.
+                7. Do Not generate the long paragarphs in response. Maximum Words should be 100.
+
+                Remember, your primary goal is to assist and educate students in the field of Artificial Intelligence. Always prioritize their learning experience and well-being."""
+    )]
+
+
+    # Zip together the past and generated messages
+    for human_msg, ai_msg in zip_longest(st.session_state['past'], st.session_state['generated']):
+        if human_msg is not None:
+            zipped_messages.append(HumanMessage(
+                content=human_msg))  # Add user messages
+        if ai_msg is not None:
+            zipped_messages.append(
+                AIMessage(content=ai_msg))  # Add AI messages
+
+    return zipped_messages
+
+
+def generate_response():
+    """
+    Generate AI response using the ChatOpenAI model.
+    """
+    # Build the list of messages
+    zipped_messages = build_message_list()
+
+    # Generate response using the chat model
+    ai_response = chat.invoke(zipped_messages)
+
+    response = ai_response.content
+
+    return response
+
+
+# Create a text input for user
+st.text_input('YOU: ', key='prompt_input', on_change=submit)
+
+
+if st.session_state.entered_prompt != "":
+    # Get user query
+    user_query = st.session_state.entered_prompt
+
+    # Append user query to past queries
+    st.session_state.past.append(user_query)
+
+    # Generate response
+    output = generate_response()
+
+    # Append AI response to generated responses
+    st.session_state.generated.append(output)
+
+
+# Display the chat history
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        # Display AI response
+        message(st.session_state["generated"][i], key=str(i))
+        # Display user message
+        message(st.session_state['past'][i],
+                is_user=True, key=str(i) + '_user')
+
+
+
